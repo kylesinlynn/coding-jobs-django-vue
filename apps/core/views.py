c829cfd4f619b8bfce915af239bbc9e59a7e7983
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-01-10 20:51:17 +0630

    👷🏻‍♂️ feat: create auth features

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 def index(request):
     return render(request, 'core/index.html')
 
 def signup(request):
-    return render(request, 'core/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+        
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'core/signup.html', {'form': form})
--- codingjobs/urls.py
+++ codingjobs/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from apps.core.views import index, signup
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
