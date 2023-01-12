44634d94828c7227c85e6e43d9586cf999ff68fb
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-01-12 07:26:05 +0630

    📊 feat: create job dashboard view

--- apps/userprofile/views.py
+++ apps/userprofile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from apps.job.models import Application
+from apps.job.models import Application, Job
 
 @login_required
 def dashboard(request):
@@ -9,5 +9,13 @@ def dashboard(request):
 
 @login_required
 def view_application(request, application_id):
-    application = get_object_or_404(Application, pk=application_id, created_by=request.user)
+    if request.user.userprofile.is_employer:
+        application = get_object_or_404(Application, pk=application_id, job__created_by=request.user)
+    else:
+        application = get_object_or_404(Application, pk=application_id, created_by=request.user)
     return render(request, 'userprofile/view_application.html', {'application': application})
+
+@login_required
+def view_dashboard_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id, created_by=request.user)
+    return render(request, 'userprofile/view_dashboard_job.html', {'job': job})
