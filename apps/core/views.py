58623d69ff32f0881e3a2e3032ad447e3df2478d
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-01-13 19:33:08 +0630

    📰 feat(view): show latest jobs first

--- apps/core/views.py
+++ apps/core/views.py
@@ -6,7 +6,7 @@ from apps.job.models import Job
 from apps.userprofile.models import Userprofile
 
 def index(request):
-    jobs = Job.objects.all()[0:3]
+    jobs = Job.objects.order_by('-created_at')[0:3]
     return render(request, 'core/index.html', {'jobs': jobs})
 
 def signup(request):
